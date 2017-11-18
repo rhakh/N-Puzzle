1d53646fbdfdc0924ec5c5153ee246293552c2b2
Roman Hakh <rhakh@e1r7p3.unit.ua>
2017-11-18 19:02:43 +0200

    [FIX] Fix build for project (all platforms)

--- gen_makefile.sh
+++ gen_makefile.sh
@@ -5,7 +5,18 @@
 # qmake -spec macx-clang
 # make
 
-qmake -project
-qmake -makefile
-PRO_FILE="`find . -name "*.pro"`"
-echo -e "\nQT += widgets gui\nCONFIG += console\n" >> $PRO_FILE
+unamestr=`uname`
+
+qmake -project "QT += widgets"
+
+if [[ $unamestr == "Darwin" ]]; then
+	qmake -spec macx-clang
+	echo "I am Darwin"
+elif [[ $unamestr == "Linux" ]]; then
+	qmake -makefile
+fi
+
+
+#PRO_FILE="`find . -name "*.pro"`"
+#echo -e "\nQT += widgets gui\nCONFIG += console\n" >> $PRO_FILE
+# echo -e 'QT += widgets gui\nCONFIG += console' | cat - $PRO_FILE > temp && mv temp $PRO_FILE
