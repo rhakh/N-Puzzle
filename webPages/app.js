df15524a80e3b3e56f6b5ff22c654b89e693102c
VR <rudakov044@gmail.com>
2018-07-21 15:12:08 +0300

    add n-puzzle img

--- webPages/app.js
+++ webPages/app.js
@@ -13,6 +13,37 @@ void function(window, document, undefined) {
         this.cells = [];
         this.finger = {};
 
-        this.init();
+        // this.init();
+        this.fillCells();
     }
+
+    Puzzle.prototype.placeCell = function(cell, i) {
+        cell.style.left = ((i % this.side) * (CELL_SIZE + CELL_GAP) + CELL_GAP) + "px";
+        cell.style.top = (Math.floor(i / this.side) * (CELL_SIZE + CELL_GAP) + CELL_GAP) + "px";
+    };
+
+    Puzzle.prototype.fillCells = function() {
+        var fragment = document.createDocumentFragment();
+        for(var i = 0; i < this.total - 1; i++) {
+            var cell = document.createElement("span");
+            cell.className = "cell";
+            cell.innerHTML = i + 1;
+            this.cells[i] = i + 1;
+            this.placeCell(cell, i);
+            fragment.appendChild(cell);
+        }
+        this.cells.push(0);
+        this.goal = this.cells.slice();
+        this.board.appendChild(fragment);
+        this.board.className = "board";
+        this.board.style.height = this.board.style.width = (this.side * (CELL_SIZE + CELL_GAP) + CELL_GAP) + "px";
+    };
+
+    Puzzle.prototype.init = function() {
+        this.fillCells();
+        // this.defineActions();
+        // this.shuffleCells();
+    };
+
+    window.Puzzle = Puzzle;
 }(window, document);
