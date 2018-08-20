c1444637b408bb91ab9622626476d6cfe9c5e378
rhakh <hakh.r.v@gmail.com>
2018-08-20 13:14:01 +0300

    Fixes in CLI parser, nMaxSwap heuristic fixed
    fixes #10

--- src/CLI.cpp
+++ src/CLI.cpp
@@ -12,22 +12,22 @@ bool	CLI::isFlagSet(const std::string &flag) const {
 	return (false);
 }
 
-int		CLI::getIntFlag(const std::string &flag) const {
+bool	CLI::getFlag(const std::string &flag, int &result) const {
 	if (this->vm.count(flag)) {
 		// trow exception if conversion failed
-		return (this->vm[flag].as<int>());
+		result = this->vm[flag].as<int>();
+		return (true);
 	}
-	throw CLI_flagNotSet();
-	return (0);
+	return (false);
 }
 
-std::string	CLI::getStringFlag(const std::string &flag) const {
+bool	CLI::getFlag(const std::string &flag, std::string &result) const {
 	if (this->vm.count(flag)) {
 		// trow exception if conversion failed
-		return (this->vm[flag].as<std::string>());
+		result = this->vm[flag].as<std::string>();
+		return (true);
 	}
-	throw CLI_flagNotSet();
-	return (0);
+	return (false);
 }
 
 bool	CLI::processArguments(int argc, char **argv) {
@@ -36,14 +36,11 @@ bool	CLI::processArguments(int argc, char **argv) {
 		po::store(po::parse_command_line(argc, argv, this->desc), this->vm);
 		po::notify(this->vm);
 
-		if (this->vm.count("verbose")) {
-			verboseLevel = this->vm["verbose"].as<int>();
-		}
+		this->getFlag("verbose", verboseLevel);
+		this->getFlag("file", fileName);
+
 		if (this->vm.count("help")) {
 			std::cout << desc << std::endl;
-		}
-		if (this->vm.count("file")) {
-			fileName = this->vm["file"].as<std::string>();
 		}
 	}
 	catch (po::error &e) {
@@ -74,8 +71,8 @@ CLI::CLI(int argc, char **argv) : desc("Options") {
 								"\t0 -- snail solution\n"
 								"\t1 -- linear solution\n")
 			("optimisation,o", po::value<int>(&optimisationByTime), "Optimisation\n"
-								"\t0 -- optimisation by time (default)\n"
-								"\t1 -- optimisation by paths' length")
+								"\t0 -- optimisation by paths' length\n"
+								"\t1 -- optimisation by time (default)")
 			("file,f", po::value<std::string>(), "File with map to solve");
 
 	if (!processArguments(argc, argv))
@@ -116,10 +113,14 @@ static NP_retVal	solvePuzzle(const int *map, int mapSize, int heuristic, int sol
 void	CLI::startLogic() const {
 	std::vector<int>	resultVector;
 	std::vector<int>	map;
-	std::ifstream		file(this->getStringFlag("file"));
+	std::string			fileName;
+	std::ifstream		file;
 	std::string			line;
 	NP_retVal			result;
 	int					mapSize, heuristic = 0, solutionType = 0;
+
+	this->getFlag("file", fileName);
+	file = std::ifstream(fileName);
 
 	if (!file.good())
 		throw CLI_InvalidFile();
@@ -149,14 +150,9 @@ void	CLI::startLogic() const {
 	if (resultVector.size() < 2)
 		throw CLI_InvalidMap();
 
-	try {
-		heuristic = this->getIntFlag("heuristic");
-		solutionType = this->getIntFlag("solution");
-		optimisationByTime = !this->getIntFlag("optimisation");
-	}
-	catch (CLI::CLI_flagNotSet &e) {
-		// use default values
-	}
+	this->getFlag("heuristic", heuristic);
+	this->getFlag("solution", solutionType);
+	this->getFlag("optimisation", optimisationByTime);
 
 	mapSize = resultVector[0];
 	for (unsigned i = 1; i < resultVector.size(); i++)
